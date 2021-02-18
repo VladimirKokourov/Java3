1ed8ca046eba6b3aef7ca19b15f1c603e2ceca26
VladimirKokourov <vkokourov@gmail.com>
2021-02-18 21:46:21 +0700

    Заглушка

--- Zagluchka.java
+++ Zagluchka.java
@@ -1,1 +1,14 @@
-Zagluchka
+package ru.geekbrains.java3.lesson1;
+
+public class Test {
+    public static void main(String[] args) {
+        int i1 = 1000;
+        int i2 = 1000;
+        System.out.println(i1 == i2);
+
+        Integer integer1 = 1000;
+        Integer integer2 = 1000;
+        System.out.println(integer1.equals(integer2));
+        System.out.println(integer1 == integer2);
+    }
+}
